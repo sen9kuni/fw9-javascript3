a794cb0eb66fa1735cf936b5af9a28c1df22589a
prayogo Santoso <prayogo.santoso681@gmail.com>
2022-06-21 20:36:31 +0700

    menambah penjelasan

--- task1.js
+++ task1.js
@@ -16,21 +16,29 @@ const cekHariKerja = (day) => {
 
 
 // then and catch
-// const tryHari = cekHariKerja('minggu')
 
-// tryHari
-//     .then((value) => console.log(`${value} adalah hari kerja`))
-//     .catch(err => console.error(err))
+// membuat fungsi yang ber isi cekHariKerja yang di isi parameter
+const tryHari = cekHariKerja('minggu')
+
+tryHari
+// jika resovle akan menghasilkan parameter yang di isi dan ditambah string 'adalah hari kerja'
+    .then((value) => console.log(`${value} adalah hari kerja`))
+    // jika reject akan menghasilkan info error
+    .catch(err => console.error(err))
 
 
 
 // try and catch
+// membuat fungsi async dengan parameter day
 async function thisDay(day) {
     console.log(`mengecek hari ${day}`)
     try {
+        // hasil akan menunggu fungsi hasil cekHariKerja dan mengembalikan nilai parameter
         const hasil = await cekHariKerja(day)
+            // saat berhasil akan menghasilkan nilai hasil + string " adalah hari kerja" 
         console.log(hasil + " adalah hari kerja")
     } catch (error) {
+        // saat gagal hanya akan menmpilkan error
         console.error(error)
     }
 }
